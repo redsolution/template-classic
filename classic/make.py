7d141302cc7734360ff8ef5a6ba2afc6e9d374c3
Alexey Kuligin <alexey.kuligin@redsolution.ru>
2010-09-29 16:38:50 +0600

    Fix syntax errors

--- classic/make.py
+++ classic/make.py
@@ -7,20 +7,21 @@ class Make(BaseMake):
         super(Make, self).premake()
         cms_settings = CMSSettings.objects.get_settings()
         template_settings = TemplateSettings.objects.get_settings()
-        cms_settings.render_to(os.path.join('..', 'templates', 'base_template.html'), 'classic/templates/base_template.html', {
+        cms_settings.render_to(['..', 'templates', 'base_template.html'], 'classic/templates/base_template.html', {
             'template_settings': template_settings,
         }, 'w')
 
     def make(self):
         super(Make, self).make()
         cms_settings = CMSSettings.objects.get_settings()
-        cms_settings.render_to(os.path.join('..', 'media', 'css', 'base.css'), 'classic/media/css/base.css', {
+        template_settings = TemplateSettings.objects.get_settings()
+        cms_settings.render_to(['..', 'media', 'css', 'base.css'], 'classic/media/css/base.css', {
             'template_settings': template_settings,
         }, 'w')
-        cms_settings.render_to(os.path.join('..', 'media', 'css', 'style.css'), 'classic/media/css/style.css', {
+        cms_settings.render_to(['..', 'media', 'css', 'style.css'], 'classic/media/css/style.css', {
             'template_settings': template_settings,
         }, 'w')
-        cms_settings.render_to(os.path.join('..', 'media', 'css', 'tinymce.css'), 'classic/media/css/tinymce.css', {
+        cms_settings.render_to(['..', 'media', 'css', 'tinymce.css'], 'classic/media/css/tinymce.css', {
             'template_settings': template_settings,
         }, 'w')
 
