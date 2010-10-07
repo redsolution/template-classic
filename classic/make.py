9c850facb9911c03f42e9faed0f53323fcf390c7
Ivan Gromov <summer.is.gone@gmail.com>
2010-10-08 02:28:16 +0600

    Added copy functionality

--- classic/make.py
+++ classic/make.py
@@ -1,6 +1,8 @@
 from redsolutioncms.make import BaseMake
 from redsolutioncms.models import CMSSettings
 from classic.models import TemplateSettings
+import os
+
 
 class Make(BaseMake):
     def premake(self):
@@ -14,15 +16,20 @@ class Make(BaseMake):
     def make(self):
         super(Make, self).make()
         cms_settings = CMSSettings.objects.get_settings()
-        template_settings = TemplateSettings.objects.get_settings()
-        cms_settings.render_to(['..', 'media', 'css', 'base.css'], 'classic/media/css/base.css', {
-            'template_settings': template_settings,
-        }, 'w')
-        cms_settings.render_to(['..', 'media', 'css', 'style.css'], 'classic/media/css/style.css', {
-            'template_settings': template_settings,
-        }, 'w')
-        cms_settings.render_to(['..', 'media', 'css', 'tinymce.css'], 'classic/media/css/tinymce.css', {
-            'template_settings': template_settings,
-        }, 'w')
+
+        cms_settings.copy_to(
+            os.path.join(cms_settings.project_dir, 'media',),
+            os.path.join(os.path.dirname(__file__), 'classic', 'media'),
+            merge=True
+        )
+        cms_settings.copy_to(
+            os.path.join(cms_settings.project_dir, 'templates',),
+            os.path.join(os.path.dirname(__file__), 'classic', 'templates'),
+            merge=True
+        )
+#        template_settings = TemplateSettings.objects.get_settings()
+#        cms_settings.render_to(['..', 'media', 'css', 'style.css'], 'classic/media/css/style.css', {
+#            'template_settings': template_settings,
+#        }, 'w')
 
 make = Make()
